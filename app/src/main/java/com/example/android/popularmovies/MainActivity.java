c1b82892c886c88149b5c99cffd1152a948cf6b2
Vivek SJ <viveksjoseph@Viveks-MacBook-Pro.local>
2018-05-22 10:32:14 -0700

    modify AsyncTask and query URL
    * Add onPostExecute for movieQueryTask
    * Create function makeMovieQuery to create URL to query and execute the
    query on a separate thread.
    * Fetch URL from strings.xml to avoid committing the API Key

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -9,17 +9,35 @@ import android.widget.TextView;
 import com.example.android.popularmovies.Utils.NetworkUtils;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mDisplayTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView displayTextView = (TextView)findViewById(R.id.movie_response);
+        mDisplayTextView = findViewById(R.id.movie_response);
+        makeMovieQuery();
+    }
 
+    private void makeMovieQuery() {
+        String movieQuery = getString(R.string.moviedb_api);
+
+        URL movieQueryUrl = null;
+        try {
+            movieQueryUrl = new URL(movieQuery);
+        } catch (MalformedURLException e) {
+            Log.d("Main Activity", "creating movieQueryUrl failed: " + e.getMessage());
+        } finally {
+            if (movieQueryUrl != null) {
+                new movieQueryTask().execute(movieQueryUrl);
+            }
+        }
     }
 
     public class movieQueryTask extends AsyncTask<URL, Void, String> {
@@ -33,6 +51,14 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("Main Activity", "getResponseFromHttpUrl failed: " + e.getMessage());
             }
             return movieQueryResults;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s != null && !s.equals("")) {
+                mDisplayTextView.setText(s);
+            }
+            super.onPostExecute(s);
         }
     }
 }
