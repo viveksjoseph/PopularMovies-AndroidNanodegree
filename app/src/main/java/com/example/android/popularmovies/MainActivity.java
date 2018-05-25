104916238010597bc6df875b32a5b445e841a74a
Vivek SJ <viveksjoseph@Viveks-MacBook-Pro.local>
2018-05-24 20:13:47 -0700

    Add GridView, Remove TextView, Create calls to API, Arrange movies in GridView
    * Add GridView and set adapters
    * Remove “Hello World” TextView
    * Create calls to API
    * Store parsed movie data in MovieData singleton
    * Arrange movies in GridView

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -4,8 +4,11 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.TextView;
+import android.widget.GridView;
 
+import com.example.android.popularmovies.Adapters.MovieDetailsAdapter;
+import com.example.android.popularmovies.Data.MovieData;
+import com.example.android.popularmovies.Utils.JsonUtils;
 import com.example.android.popularmovies.Utils.NetworkUtils;
 
 import java.io.IOException;
@@ -14,23 +17,20 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView mDisplayTextView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDisplayTextView = findViewById(R.id.movie_response);
         makeMovieQuery();
     }
 
     private void makeMovieQuery() {
-        String movieQuery = getString(R.string.moviedb_api);
-
         URL movieQueryUrl = null;
+
         try {
-            movieQueryUrl = new URL(movieQuery);
+            movieQueryUrl = NetworkUtils.BuildQueryURL(this,
+                    MovieData.getInstance().getCurrentGridArrangement().toString());
         } catch (MalformedURLException e) {
             Log.d("Main Activity", "creating movieQueryUrl failed: " + e.getMessage());
         } finally {
@@ -55,10 +55,21 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String s) {
-            if (s != null && !s.equals("")) {
-                mDisplayTextView.setText(s);
-            }
             super.onPostExecute(s);
+
+            if (s != null && !s.equals("")) {
+                MovieData.getInstance().movieDetailsArray = JsonUtils.parseMovieResponseJson(s);
+
+                if (MovieData.getInstance().movieDetailsArray == null) {
+                    return;
+                }
+                MovieDetailsAdapter adapter = new MovieDetailsAdapter(getApplicationContext(),
+                        MovieData.getInstance().movieDetailsArray);
+
+                GridView gridView = findViewById(R.id.movies_gridview);
+                gridView.setAdapter(adapter);
+
+            }
         }
     }
 }
