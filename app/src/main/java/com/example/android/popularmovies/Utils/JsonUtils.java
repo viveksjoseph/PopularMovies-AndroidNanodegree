bec75cd0196d4d7c7480e77f6544ac70029f774c
Vivek SJ <viveksjoseph@viveks-mbp.lan>
2018-06-04 22:39:55 -0700

    Cleanup GSON implementation
    * Cleanup GSON implementation
    * Add JSONParseException to rejigger exceptions

--- app/src/main/java/com/example/android/popularmovies/Utils/JsonUtils.java
+++ app/src/main/java/com/example/android/popularmovies/Utils/JsonUtils.java
@@ -3,15 +3,14 @@ package com.example.android.popularmovies.Utils;
 import com.example.android.popularmovies.Data.MovieResponse;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParseException;
 
 public class JsonUtils {
 
-    public static MovieResponse parseMovieResponseJson(String json) throws Exception {
+    public static MovieResponse parseMovieResponseJson(String json) throws JsonParseException {
         Gson movieGson = new GsonBuilder()
                 .setDateFormat("yyyy-mm-dd")
                 .create();
-        MovieResponse moviesArray = movieGson.fromJson(json, MovieResponse.class);
-        return moviesArray;
-
+        return movieGson.fromJson(json, MovieResponse.class);
     }
 }
