422a5897703596510988fe2f35a5c3cd3c25b094
Vivek SJ <viveksjoseph@viveks-mbp.lan>
2018-07-25 23:43:30 -0700

    handle calls of review and video

--- app/src/main/java/com/example/android/popularmovies/Utils/NetworkUtils.java
+++ app/src/main/java/com/example/android/popularmovies/Utils/NetworkUtils.java
@@ -2,6 +2,7 @@ package com.example.android.popularmovies.Utils;
 
 import android.content.Context;
 import android.net.Uri;
+import android.util.Log;
 
 import com.example.android.popularmovies.R;
 
@@ -17,10 +18,13 @@ public class NetworkUtils {
 
     private static final String MOVIEDB_BASE_URL = "https://api.themoviedb.org/3";
     private static final String MOVIEDB_MOVIE_PATH = "movie";
+    private static final String MOVIEDB_VIDEO_PATH = "videos";
+    private static final String MOVIEDB_REVIEW_PATH = "reviews";
     private static final String MOVIEDB_APIKEY_QUERY = "api_key";
 
     // return entire result from HTTP response.
     public static String getResponseFromHttpUrl(URL url) throws IOException {
+        Log.d("NetworkUtils", "Attempting to connect to url: " + url.toString());
         OkHttpClient urlConnectionClient = new OkHttpClient();
         Request connectionRequest = new Request.Builder()
                 .url(url)
@@ -43,6 +47,28 @@ public class NetworkUtils {
         Uri queryUri = Uri.parse(MOVIEDB_BASE_URL).buildUpon()
                 .appendPath(MOVIEDB_MOVIE_PATH)
                 .appendPath(sortPreference)
+                .appendQueryParameter(MOVIEDB_APIKEY_QUERY, context.getString(R.string.moviedb_api))
+                .build();
+
+        return queryUri.toString();
+    }
+
+    public static String BuildVideoURL(Context context, long movieId){
+        Uri queryUri = Uri.parse(MOVIEDB_BASE_URL).buildUpon()
+                .appendPath(MOVIEDB_MOVIE_PATH)
+                .appendPath(String.valueOf(movieId))
+                .appendPath(MOVIEDB_VIDEO_PATH)
+                .appendQueryParameter(MOVIEDB_APIKEY_QUERY, context.getString(R.string.moviedb_api))
+                .build();
+
+        return queryUri.toString();
+    }
+
+    public static String BuildReviewURL(Context context, long movieId){
+        Uri queryUri = Uri.parse(MOVIEDB_BASE_URL).buildUpon()
+                .appendPath(MOVIEDB_MOVIE_PATH)
+                .appendPath(String.valueOf(movieId))
+                .appendPath(MOVIEDB_REVIEW_PATH)
                 .appendQueryParameter(MOVIEDB_APIKEY_QUERY, context.getString(R.string.moviedb_api))
                 .build();
 
