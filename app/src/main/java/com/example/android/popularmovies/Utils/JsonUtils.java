b1643154c7140bb467e58f8255b58ba4d6ad1ef2
Vivek SJ <viveksjoseph@viveks-mbp.lan>
2018-07-25 23:43:15 -0700

    Handle parsing of video and review

--- app/src/main/java/com/example/android/popularmovies/Utils/JsonUtils.java
+++ app/src/main/java/com/example/android/popularmovies/Utils/JsonUtils.java
@@ -1,6 +1,8 @@
 package com.example.android.popularmovies.Utils;
 
-import com.example.android.popularmovies.Data.MovieResponse;
+import com.example.android.popularmovies.Data.Movies.MovieResponse;
+import com.example.android.popularmovies.Data.Reviews.ReviewResponse;
+import com.example.android.popularmovies.Data.Videos.VideoResponse;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonParseException;
@@ -12,5 +14,15 @@ public class JsonUtils {
                 .setDateFormat("yyyy-mm-dd")
                 .create();
         return movieGson.fromJson(json, MovieResponse.class);
+    }
+
+    public static VideoResponse parseVideoResponseJson(String json) throws  JsonParseException{
+        Gson videoGson = new GsonBuilder().create();
+        return videoGson.fromJson(json, VideoResponse.class);
+    }
+
+    public static ReviewResponse parseReviewResponseJson(String json) throws  JsonParseException{
+        Gson reviewGson = new GsonBuilder().create();
+        return reviewGson.fromJson(json, ReviewResponse.class);
     }
 }
